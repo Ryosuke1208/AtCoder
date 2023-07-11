4e86dc730486ebcf0185be158e000f3ad97a78f7
Ryosuke1208 <miyuki.ryosuke3394@gmail.com>
2023-07-12 06:19:37 +0900

    2023/7/12

---
+++ AtCoder Problems/AGC_029_A_Irreversible_operation.cpp
@@ -0,0 +1,56 @@
+/********************tips********************/
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+void _main();
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    _main();
+}
+typedef long long ll;
+const int inf = INT_MAX / 2;
+const ll infl = 1LL << 60;
+template <class T> bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T> bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+/********************************************/
+
+/********************code********************/
+void _main() {
+    // 最初作った時またもやllとintでエラー
+    // しっかりとしなければいけない
+    string S;
+    cin >> S;
+    ll n = S.length();
+    ll cnt = 0;
+    ll ans = 0;
+    rep(i, 0, n) {
+        if (S[i] == 'W')
+            ans += cnt;
+        else
+            cnt++;
+    }
+
+    cout << ans << endl;
+    return;
+}
+
+/***************example_answer***************/
+/*
+
+*/
